d9be1c7bbb087d0a25fe85291bd609420e888967
Camilo Sampedro <sampedro1903@gmail.com>
2017-10-05 22:04:13 -0500

    Minor refactoring

--- src/test/scala/io/camilosampedro/lambda/JsonSample.scala
+++ src/test/scala/io/camilosampedro/lambda/JsonSample.scala
@@ -1,6 +1,4 @@
 package io.camilosampedro.lambda
-import io.circe._
 import io.circe.generic.JsonCodec
-import io.circe.generic.semiauto._
 
 @JsonCodec case class JsonSample(fieldNameA: String, fieldNameB: Int, fieldNameC: Boolean)
